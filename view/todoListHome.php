f78cc13a3eb2c341d483190d7c4b407fa3c38ad8
Luca <luca.bassi@cpnv.ch>
2020-03-12 16:48:00 +0100

    1/2 - ok

--- view/todoListHome.php
+++ view/todoListHome.php
@@ -4,48 +4,140 @@ ob_start();
 $title = "CSU-NVB - Tâches hebdomadaires";
 ?>
 
-<?php
-$index = 0;
 
+<style>
 
+    /* DivTable.com */
+    .divTable {
+        display: table;
+        width: 100%;
+    }
 
-foreach ($results  as $result){
-    ?>
-    <div>
-        <?php
+    .divTableRow {
+        display: table-row;
+    }
 
-        echo $result["lundi"][0]["id"];
-        echo $result ["lundi"]['date'];
-        echo $result ["lundi"]['base'];
-        echo $result ["lundi"]['nightjob'];
-        echo $result ["lundi"]['description'];
-        echo $result ["lundi"]['acknowledged_by'];
-        echo $result ["lundi"]['type'];
-        echo $result ["lundi"]['value'];
-        ?>
-        <form action="index.php?action=selctedItem" method=post>
-            <input type="hidden" value="<?= $result ['id'] ?>" name="idItem">
-            <input type="hidden" value="<?= $result ['base'] ?>" name="base">
-            <button type="submit"> Voir</button>
-        </form>
+    .divTableHeading {
+        background-color: #EEE;
+        display: table-header-group;
+    }
+
+    .divTableCell, .divTableHead {
+        border: 1px solid #999999;
+        display: table-cell;
+        padding: 3px 10px;
+    }
+
+    .divTableHeading {
+        background-color: #EEE;
+        display: table-header-group;
+        font-weight: bold;
+    }
+
+    .divTableFoot {
+        background-color: #EEE;
+        display: table-footer-group;
+        font-weight: bold;
+    }
+
+    .divTableBody {
+        display: table-row-group;
+    }
+</style>
+
+<div class="divTable">
+    <div class="divTableHeading">
+        <div class="divTableRow">
+            <div class="divTableHead">Lundi</div>
+            <div class="divTableHead">Mardi</div>
+            <div class="divTableHead">Marcredi</div>
+            <div class="divTableHead">Jeudi</div>
+            <div class="divTableHead">Vendredi</div>
+            <div class="divTableHead">Samedi</div>
+            <div class="divTableHead">Dimanche</div>
+        </div>
     </div>
-    <?php
-}
-
-?>
-
-<br>
-<br>
-<br>
-<form method="post" action="index.php?action=adminPage">
-    <button>
-        Administrer les taches
-    </button>
-</form>
-<button type="button" href="view/newTask">
-    Ajouter une tache
-</button>
+    <div class="divTableBody">
+        <div class="divTableRow">
 <?php
-$content = ob_get_clean();
-require "gabarit.php";
+foreach ($results as $result) {
+foreach ($result as $todo) {
+    ?>
+    <div   class="divTableCell">
+    <?php
+foreach ($todo as $values) {
+foreach ($values as $key => &$value) {
 ?>
+
+
+            <?= $value ?>
+    <br>
+<!--            <div class="divTableCell">$1</div>
+            <div class="divTableCell">3</div>
+            <div class="divTableCell">$3</div>
+        </div>
+        <div class="divTableRow">
+            <div class="divTableCell">Banana</div>
+            <div class="divTableCell">$1.5</div>
+            <div class="divTableCell">2</div>
+            <div class="divTableCell">$3</div>
+        </div>
+        <div class="divTableRow">
+            <div class="divTableCell">Ananas</div>
+            <div class="divTableCell">$2</div>
+            <div class="divTableCell">1</div>
+            <div class="divTableCell">$2</div>
+        </div>-->
+
+
+
+
+
+
+    <?php } ?>
+
+    <form action="index.php?action=selctedItem" method=post>
+        <input type="hidden" value="<?/*= $result ['id'] */ ?>" name="idItem">
+        <input type="hidden" value="<?/*= $result ['base'] */ ?>" name="base">
+        <button type="submit"> Voir</button>
+    </form>
+    <br>
+    <?php
+
+    }
+    ?> </div> <?php
+    }
+?> </div> <?php
+    }
+    ?>
+
+
+
+
+
+        <div class="divTableFoot">
+            <div class="divTableRow">
+                <div class="divTableCell">Sum</div>
+                <div class="divTableCell">&nbsp;</div>
+                <div class="divTableCell">6</div>
+                <div class="divTableCell">$7</div>
+            </div>
+        </div>
+    </div>
+
+
+    <br>
+    <br>
+    <br>
+    <form method="post" action="index.php?action=adminPage">
+        <button>
+            Administrer les taches
+        </button>
+    </form>
+    <button type="button" href="view/newTask">
+        Ajouter une tache
+    </button>
+    <?php
+    $content = ob_get_clean();
+    require "gabarit.php";
+    ?>
